04e5b743a734309342991d820cb9ff2a694309dd
belion-freee <belion.freee@gmail.com>
2019-07-31 14:54:25 +0900

    require rb files

--- lib/bolo.rb
+++ lib/bolo.rb
@@ -1,6 +1,14 @@
-require "bolo/version"
+# require bolo/*rb
+Dir["#{File.dirname(__FILE__)}/bolo/*.rb"].each do |path|
+  next if File.basename(path, '.rb') == 'core_ext'
+  require "bolo/#{File.basename(path, '.rb')}"
+end
+
+# require bolo/core_ext/*rb
+Dir["#{File.dirname(__FILE__)}/bolo/core_ext/*.rb"].each do |path|
+  require "bolo/core_ext/#{File.basename(path, '.rb')}"
+end
 
 module Bolo
-  class Error < StandardError; end
-  # Your code goes here...
+  class BoloError < StandardError; end
 end
